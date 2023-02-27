efa0088c231595764942102b8691c3080eed07e3
luis arthur <luisarthurlards03@gmail.com>
2023-02-27 16:35:31 -0300

    adição do react router

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,15 +1,28 @@
 import React from 'react';
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
 import rockGlass from './images/rockGlass.svg';
 
 function App() {
   return (
-    <div className="App">
-      <span className="logo">TRYBE</span>
-      <object className="rocksGlass" type="image/svg+xml" data={ rockGlass }>
-        Glass
-      </object>
-    </div>
+    <Switch>
+      <Route
+        exact
+        path="/"
+        component={ Login }
+      />
+      <Route
+        exact
+        path="/search"
+        component={ Search }
+      />
+
+      <Route
+        exact
+        path="*"
+        component={ NotFound }
+      />
+    </Switch>
   );
 }
 
