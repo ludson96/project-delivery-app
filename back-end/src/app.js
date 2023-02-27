3a5478e8f84ab9f4a8f046b06744dd1b7e808bac
Ludson96 <ludson_ps27@hotmail.com>
2023-02-27 19:05:20 -0300

    fix: I corrected wrong route

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const { userRouter } = require('./routers');
+const { userRouter } = require('./routers/user.router');
 
 const app = express();
 
 app.use(express.json());
 
-app.use('/user', userRouter);
+app.use('/register', userRouter);
 
 module.exports = app;
