3d29d19178f800d5c4d8c2b495c14ba9f4c87e6b
Ludson96 <ludson_ps27@hotmail.com>
2023-02-28 14:23:32 -0300

    fix: bug

--- back-end/src/controllers/user.controller.js
+++ back-end/src/controllers/user.controller.js
@@ -1,19 +1,19 @@
-const UserService = require('../services/user.service');
+// const UserService = require('../services/user.service');
 
-  const createUser = async (req, res) => {
-    try {
-      const newUser = req.body;
-      const { user, token } = await UserService.createUser(newUser);
-      if (!user) return res.status(409).json({ message: 'User already registered' });
-      return res.status(201).json({ token });
-    } catch (erro) {
-      return res.status(500).json({
-        message: 'Erro ao criar usuário no banco',
-        error: erro.message,
-      });
-    }
-  };
+//   const createUser = async (req, res) => {
+//     try {
+//       const newUser = req.body;
+//       const { user, token } = await UserService.createUser(newUser);
+//       if (!user) return res.status(409).json({ message: 'User already registered' });
+//       return res.status(201).json({ token });
+//     } catch (erro) {
+//       return res.status(500).json({
+//         message: 'Erro ao criar usuário no banco',
+//         error: erro.message,
+//       });
+//     }
+//   };
 
-  module.exports = {
-    createUser,
-  };
+//   module.exports = {
+//     createUser,
+//   };
