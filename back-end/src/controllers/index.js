92dc89eb39dbff246de4394377e36c060d51a0cb
Lucas Israel <lisraelalves92@gmail.com>
2023-03-01 15:57:31 -0300

    feat: :sparkles: route for products

--- back-end/src/controllers/index.js
+++ back-end/src/controllers/index.js
@@ -1,7 +1,9 @@
 const UserController = require('./user.controller');
 const LoginController = require('./login.controller');
+const ProductController = require('./product.controller');
 
 module.exports = {
   UserController,
   LoginController,
+  ProductController,
 };
--- back-end/src/routers/index.js
+++ back-end/src/routers/index.js
@@ -1,7 +1,9 @@
 const userRouter = require('./user.router');
 const LoginRouter = require('./login.router');
+const ProductRouter = require('./products.router');
 
 module.exports = {
   userRouter,
   LoginRouter,
+  ProductRouter,
 };
