eb87ee714daebc3effba488d1a2a1b00b9a9df24
luis arthur <luisarthurlards03@gmail.com>
2023-03-01 17:50:44 -0300

    products.mocks

---
+++ back-end/src/tests/mocks/products.mocks.js
@@ -0,0 +1,15 @@
+const producs = [
+  {
+    id: 1,
+    name: "Skol Lata 250ml",
+    price: 2.2,
+    urlImage: "http://localhost:3001/images/skol_lata_350ml.jpg"
+  },
+  {
+    id: 2,
+    name: "Heineken 600ml",
+    price: 7.5,
+    urlImage: "http://localhost:3001/images/heineken_600ml.jpg"
+  }]
+
+module.exports ={producs}
