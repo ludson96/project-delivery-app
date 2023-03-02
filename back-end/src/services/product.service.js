70215fb83aa9f29748be6d70fb94ee17414008a2
Lucas Israel <lisraelalves92@gmail.com>
2023-03-02 19:37:53 -0300

    refactor: :art: removing a function call

--- back-end/src/services/product.service.js
+++ back-end/src/services/product.service.js
@@ -3,21 +3,15 @@ const { Product } = require('../database/models');
 const getProducts = async () => {
   const result = await Product.findAll();
 
-  const result2 = result.map((item) => {
+  const result2 = result.map(({ id, name, price, urlImage }) => ({
+      id,
+      name,
+      price: price.toFixed(2).toString(),
+      urlImage,
+    }));
 
-    const result = item.dataValues
-
-    result.price.toFixed(2);
-
-    return result
-  });
-
-  console.log(result2);
-  
   return result2;
 };
-
-getProducts()
 
 module.exports = {
   getProducts,
