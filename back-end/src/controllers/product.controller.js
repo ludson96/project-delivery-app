6b3f1d1687dc37ae5920da633dbdcdb9ef4a6db0
Lucas Israel <lisraelalves92@gmail.com>
2023-03-03 16:42:27 -0300

    feat: :sparkles: status error changes based on what is received from the service.

--- back-end/src/controllers/product.controller.js
+++ back-end/src/controllers/product.controller.js
@@ -1,11 +1,12 @@
 const { ProductService } = require('../services/Product.service');
+const { getStatusCode } = require('./helpers/htmlcodes');
 
 const prodService = new ProductService();
 
 const getProducts = async (req, res) => {
   try {
     const { type, payload } = await prodService.getProducts();
-    if (type) return res.status(404).json({ message: 'Product not found' });
+    if (type) return res.status(getStatusCode(type)).json({ message: 'Product not found' });
     return res.status(200).json(payload);
   } catch (erro) {
     return res.status(500).json({
