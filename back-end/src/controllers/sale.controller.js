7323a2e4d6904c6e4983fcb2e040d4723567242a
Lucas Israel <lisraelalves92@gmail.com>
2023-03-07 17:30:05 -0300

    feat: :sparkles: changing logic to create a new sale and to add products in saleProducts

--- back-end/src/controllers/sale.controller.js
+++ back-end/src/controllers/sale.controller.js
@@ -1,16 +1,24 @@
 const { SaleService } = require('../services/Sale.service');
+const { SaleProduct } = require('../services/SaleProduct.service');
 
 class SaleController {
   constructor() {
-    this.service = new SaleService();
+    this.SaleService = new SaleService();
+    this.SaleProductService = new SaleProduct();
     this.createSale = this.createSale.bind(this);
   }
 
   async createSale(req, res) {
     try {
       const saleBody = req.body;
-      const result = await this.service.createSale(saleBody);
-      return res.status(201).json(result.payload);
+      const result0 = await this.SaleService.createSale(saleBody);
+      await Promise.all(saleBody.products.map(({ productId, quantity }) => this
+        .SaleProductService.createSaleProduct({
+          saleId: result0.payload.id,
+          productId,
+          quantity,
+        })));
+      return res.status(201).json({ saleId: result0.payload.id });
     } catch (erro) {
       return res.status(500).json({
         message: 'Erro ao criar uma venda no banco',
