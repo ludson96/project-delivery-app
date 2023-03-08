1e8414b61b6045a04ab183f1444539ac5c61b471
Lucas Israel <lisraelalves92@gmail.com>
2023-03-08 16:17:22 -0300

    feat: :sparkles: adding a validation for userBody when creating a new user

---
+++ back-end/src/middlewares/registerUserBody.js
@@ -0,0 +1,10 @@
+const validateUserBody = (req, res, next) => {
+  if (!req.body.name) return res.status(400).json({ message: 'Body needs a name key' });
+  if (!req.body.email) return res.status(400).json({ message: 'Body needs an email key' });
+  if (!req.body.password) return res.status(400).json({ message: 'Body needs a password' });
+  next();
+};
+
+module.exports = {
+  validateUserBody,
+};
