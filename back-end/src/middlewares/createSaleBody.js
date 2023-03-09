b2e84ed60fcbc0392008f1787f2226fa0f1c7c5e
Lucas Israel <lisraelalves92@gmail.com>
2023-03-08 22:44:43 -0300

    feat: :sparkles: body validation for creating a sale

--- back-end/src/middlewares/createSaleBody.js
+++ back-end/src/middlewares/createSaleBody.js
@@ -1,29 +1,72 @@
-const saleBodyKeys = (req, res, next) => {
-  if (!req.body.totalPrice) return res.status(400).json({ message: 'Body needs a totalPrice key' });
-  if (!req.body.deliveryAddress) {
-    return res.status(400).json({
-        message: 'Body needs a deliveryAddress key',
-      }); 
-  }
-  if (!req.body.deliveryNumber) {
-    return res.status(400).json({
-      message: 'Body needs a deliveryNumber key',
-    }); 
-  }
-  if (!req.body.products) return res.status(400).json({ message: 'Body needs a products key' });
-  next();
-};
-
-const saleBodyProductsArray = (req, res, next) => {
-  if (!Array.isArray(req.body.products)) {
-  return res.status(400)
-    .json({ message: 'Body needs the products key to be an array' }); 
+class ValidatingBody {
+  constructor(keyList) {
+    this.keyList = keyList;
+    this.checkingBodyContents = this.checkingBodyContents.bind(this);
   }
 
-  next();
-};
+  static keyChecker(reqWithBody, listOfKeys) {
+    const list = [];
+    let message;
+  
+    listOfKeys.forEach((item) => {
+      if (!(item in reqWithBody.body)) list.push(item);
+    });
+    
+    const listSizeOne = list.length === 1;
+    const lstSzHgrOne = list.length > 1;
+  
+    if (listSizeOne) message = (`Body is missing the key ${list}.`).replaceAll(',', ', ');
+    if (lstSzHgrOne) message = (`Body is missing the keys ${list}.`).replaceAll(',', ', ');
+  
+    return { message };
+  }
+
+  static saleBodyProductsArray(reqWithBody) {
+    const arrayCheck = reqWithBody.body.products;
+    let message;
+
+    if (!Array.isArray(arrayCheck)) {
+      message = 'Body needs the products key to be an array'; 
+      return;
+    }
+
+    const isobj = arrayCheck.some((item) => Object.keys(item).length > 0);
+
+    if (!isobj) {
+      message = 'Values inside array needs to be an object';
+      return;
+    }
+
+    return { message };
+  }
+
+  static objsInArrayCheck(reqWithBody) {
+    const arrayCheck = reqWithBody.body.products;
+    let message;
+  
+    arrayCheck.forEach((item) => {
+      if (!('productId' in item)) message = 'A Key of an object in array needs to be productId';
+      
+      if (!('quantity' in item)) message = 'A Key of an object in array needs to be quantity';
+    });
+  
+    return { message };
+  }
+
+  checkingBodyContents(req, res, next) {
+    const one = ValidatingBody.keyChecker(req, this.keyList);
+    if (one.message) return res.status(400).json({ message: one.message });
+  
+    const two = ValidatingBody.saleBodyProductsArray(req);
+    if (two.message) return res.status(400).json({ message: two.message });
+  
+    const three = ValidatingBody.objsInArrayCheck(req);
+    if (three.message) return res.status(400).json({ message: three.message });
+  
+    next();
+  }
+}
 
 module.exports = {
-  saleBodyKeys,
-  saleBodyProductsArray,
+  ValidatingBody,
 };
--- back-end/src/routers/sale.router.js
+++ back-end/src/routers/sale.router.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const { SaleController } = require('../controllers');
 const { tokenValidation } = require('../middlewares/tokenValidation');
-const { saleBodyKeys, saleBodyProductsArray } = require('../middlewares/createSaleBody');
+const { ValidatingBody } = require('../middlewares/createSaleBody');
 
 const router = express.Router();
 
+const listOfKeysToValidate = ['totalPrice', 'deliveryAddress', 'deliveryNumber', 'products'];
+
 router.post(
-  '/', tokenValidation, saleBodyKeys, saleBodyProductsArray, new SaleController().createSale,
+  '/',
+  tokenValidation,
+  new ValidatingBody(listOfKeysToValidate).checkingBodyContents,
+  new SaleController().createSale,
 );
 router.get('/', tokenValidation, new SaleController().getSales);
 
