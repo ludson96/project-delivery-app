533d60d879121e8d01588ba9c86d288202f4a1c3
luis arthur <luisarthurlards03@gmail.com>
2023-03-09 15:10:47 -0300

    importação getMineSales

--- front-end/src/components/CardOrders.jsx
+++ front-end/src/components/CardOrders.jsx
@@ -2,33 +2,33 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
-function CardOrders({ id, order, status, date, price }) {
+function CardOrders({ id, order, status, date, price, index }) {
   return (
-    <Link to={ `/customer/orders/${order}` }>
+    <Link to={ `/customer/orders/${id}` }>
       <div>
         <p>Pedido</p>
         <p
           data-testid={ `customer_orders__element-order-id-${id}` }
         >
-          { order }
+          { id }
         </p>
       </div>
       <div>
         <div>
           <h2
-            data-testid={ `customer_orders__element-delivery-status-${id}` }
+            data-testid={ `customer_orders__element-delivery-status-${index}` }
           >
             { status }
           </h2>
         </div>
         <div>
           <h3
-            data-testid={ `customer_orders__element-order-date-${id}` }
+            data-testid={ `customer_orders__element-order-date-${index}` }
           >
             { date }
           </h3>
           <h3
-            data-testid={ `customer_orders__element-card-price-${id}` }
+            data-testid={ `customer_orders__element-card-price-${index}` }
           >
             { price }
           </h3>
@@ -44,6 +44,7 @@ CardOrders.propTypes = {
   status: PropTypes.string.isRequired,
   date: PropTypes.string.isRequired,
   price: PropTypes.string.isRequired,
+  index: PropTypes.number.isRequired,
 };
 
 export default CardOrders;
